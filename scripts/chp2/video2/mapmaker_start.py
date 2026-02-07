0f315f28ce3bc5a83dec5a74b7cb78d72118e24f
Ariadna Blanca Romero <ariadna@starignus.com>
2026-02-07 16:32:18 +0000

    Adding first code and test

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,14 @@
+"""
+ For this example, we will define a module to create points that consist of a city name, its latitude and longitude coordinates.
+"""
+
+
 class Point():
-    pass
+    def __init__(self, name, latitude, longitude):
+        # so they are not accessible to the user we use "_"
+        self._name = name
+        self._latitude = latitude
+        self.longitude = longitude
+   
+    def get_lat_log(self):
+        return self._latitude, self.longitude
--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,4 +1,10 @@
+from scripts.chp2.video2.mapmaker_start import Point
 
 
 def test_make_one_point():
-    pass
+    """
+    Create a point for the city of San Francisco,
+    which is located at 37.77 degrees latitude and -122.42 degrees longitude.
+    """
+    point1 = Point("San Francisco", 37.77, -122.42)
+    assert point1.get_lat_log() == (37.77, -122.42)
